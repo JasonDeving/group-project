5ca0f0117e6ab8d6db841f4b4a5bf9385d17def3
Admin <admin@Admins-MacBook-Pro.local>
2016-08-13 13:34:24 -0700

    up on github

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+var queryURL = "http://api.undata-api.org/organizations?format=json&app_id=dacca604&app_key=2ad8e773f78a38755c09c0a9841e393d";
+// https://api.seer.cancer.gov/rest/disease/latest?type=HEMATO&q=Burkitt
+console.log(queryURL);
+$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+	console.log(response[0].full_name);
+});
