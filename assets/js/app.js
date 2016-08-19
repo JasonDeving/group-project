534ee65eae9082e46dfac84b46b1b071bd633fc0
Admin <admin@Admins-MacBook-Pro.local>
2016-08-18 19:04:20 -0700

    push up

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,13 @@ var queryURL = "https://restcountries.eu/rest/v1/all";
 console.log(queryURL);
 $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
 	for(var i = 0; response.length > i; i++) {
-		console.log(response[i].name)
-		console.log(response[i].capital)
-		console.log(response[i].timezones)
-		console.log(response[i].latlng[0] + " , " + response[i].latlng[1])
-		console.log(response[i].altSpellings[0].toLowerCase())
+		console.log(response[i].name);
+		console.log(response[i].capital);
+		console.log(response[i].timezones[0]);
+		console.log(response[i].latlng[0] + " , " + response[i].latlng[1]);
+		console.log(response[i].altSpellings[0].toLowerCase());
+		console.log(response[i].currencies[0]);
+		console.log(response[i].languages[0]);
 	}
 });
 
