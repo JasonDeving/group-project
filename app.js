5eb92c1e45560c50c5a6567b2fb141f0c23b7644
jasondeving <aznj50@hotmail.com>
2016-08-19 18:11:14 -0700

    add

--- app.js
+++ app.js
@@ -1,7 +1,49 @@
-var queryURL = "https://restcountries.eu/rest/v1/all";
-console.log(queryURL);
-$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
-	console.log(response);
-});
-
 // var googlemapapiKEY = "AIzaSyAAjy2pGN7KvOJ12ZmSqWsq7Up6iV7IBPU";
+
+var queryURL = "https://restcountries.eu/rest/v1/all";
+$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+	// beginnning of for loop
+	// for(var i = 0; response.length > i; i++) {
+
+	// 	console.log(response[i].name);
+	// 	console.log(response[i].capital);
+	// 	console.log(response[i].timezones[0]);
+	// 	console.log(response[i].latlng[0] + " , " + response[i].latlng[1]);
+	// 	console.log(response[i].altSpellings[0].toLowerCase());
+	// 	console.log(response[i].currencies[0]);
+	// 	console.log(response[i].languages[0]);
+	
+	//  function initMap() {
+ //        var blah = {lat: 0, lng: 0};
+ //        var map = new google.maps.Map(document.getElementById('map-canvas'), {
+ //          zoom: 4,
+ //          center: blah
+ //        });
+
+ //        var contentString = '<div id="content">'+
+ //            '<div id="siteNotice">'+
+ //            '</div>'+ '<h1>' + response[i].name + '</h1>' +
+ //            '<ul>'+
+ //            '<li>' + 'Capital: ' + response[i].capital + '</li>'
+ //            '<li>' + 'Time Zone: ' + response[i].timezones[0] + '</li>'
+ //            '<li>' + 'Language Spoken: ' + response[i].languages[0] + '</li>'
+ //            '</ul>'+
+ //            '</div>'+
+ //            '</div>';
+
+ //        var infowindow = new google.maps.InfoWindow({
+ //          content: contentString
+ //        });
+
+ //        var marker = new google.maps.Marker({
+ //          position: response[i].name,
+ //          map: map,
+ //          title: 'Country: ' + response[i].name
+ //        });
+ //        marker.addListener('click', function() {
+ //          infowindow.open(map, marker);
+ //        });
+ //      }
+ //      // end of forloop
+ //  }
+});
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,75 @@
-var queryURL = "https://restcountries.eu/rest/v1/all";
-console.log(queryURL);
-$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
-	for(var i = 0; response.length > i; i++) {
-		console.log(response[i].name);
-		console.log(response[i].capital);
-		console.log(response[i].timezones[0]);
-		console.log(response[i].latlng[0] + " , " + response[i].latlng[1]);
-		console.log(response[i].altSpellings[0].toLowerCase());
-		console.log(response[i].currencies[0]);
-		console.log(response[i].languages[0]);
-	}
-});
-
-// var googlemapapiKEY = "AIzaSyAAjy2pGN7KvOJ12ZmSqWsq7Up6iV7IBPU";
-
-
-
-
-
-
-
-
-function initialize() {
-  var mapOptions = {
-     center: new google.maps.LatLng(-34.397, 150.644),
-     zoom: 1,
-     minZoom: 1
+// This example displays a marker at the center of Australia.
+// When the user clicks the marker, an info window opens.
+function initMap() {
+  
+  var broadway = {
+    info: '<strong>Chipotle on Broadway</strong><br>\
+          5224 N Broadway St<br> Chicago, IL 60640<br>\
+          <a href="https://goo.gl/maps/jKNEDz4SyyH2">Get Directions</a>',
+    lat: 41.976816,
+    long: -87.659916
   };
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+  var belmont = {
+    info: '<strong>Chipotle on Belmont</strong><br>\
+          1025 W Belmont Ave<br> Chicago, IL 60657<br>\
+          <a href="https://goo.gl/maps/PHfsWTvgKa92">Get Directions</a>',
+    lat: 41.939670,
+    long: -87.655167
+  };
+
+  var sheridan = {
+    info: '<strong>Chipotle on Sheridan</strong><br>\r\
+          6600 N Sheridan Rd<br> Chicago, IL 60626<br>\
+          <a href="https://goo.gl/maps/QGUrqZPsYp92">Get Directions</a>',
+    lat: 42.002707,
+    long: -87.661236
+  };
+
+  var locations = [
+      [broadway.info, broadway.lat, broadway.long, 0],
+      [belmont.info, belmont.lat, belmont.long, 1],
+      [sheridan.info, sheridan.lat, sheridan.long, 2],
+    ];
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: new google.maps.LatLng(41.976816, -87.659916),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  var infowindow = new google.maps.InfoWindow({});
+
+  var marker, i;
+
+  for (i = 0; i < locations.length; i++) {
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+      map: map
+    });
+
+    google.maps.event.addListener(marker, 'click', (function (marker, i) {
+      return function () {
+        infowindow.setContent(locations[i][0]);
+        infowindow.open(map, marker);
+      }
+    })(marker, i));
+  }
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+  
+    var queryURL = "https://restcountries.eu/rest/v1/all", response;
+    $.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+    		// beginnning of for loop
+    	for(var i = 0; response.length > i; i++) {
+
+    		console.log(response[i].name);
+    		console.log(response[i].capital);
+    		console.log(response[i].timezones[0]);
+    		console.log(response[i].latlng[0] + " , " + response[i].latlng[1]);
+    		console.log(response[i].altSpellings[0].toLowerCase());
+    		console.log(response[i].currencies[0]);
+    		console.log(response[i].languages[0]);
+      }
+  });
+// END FUNCTION
